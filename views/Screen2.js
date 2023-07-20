17fb336d1c2aa49de33585f2835624847e309f85
Dash10107 <dakshcjain@gmail.com>
2023-07-20 10:34:18 +0530

    Properly edited and working download

--- views/Screen2.js
+++ views/Screen2.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
+
 import { useRecoilState } from 'recoil';
 import { InputText } from '../atoms';
 
@@ -14,6 +17,7 @@ function Screen2({ route }) {
     const image = JSON.parse(clickedImage);
    
     setImageData(image);
+    // console.log(image);
     const imageURL = image?.cover_photo.urls.regular;
     setImageUrl(imageURL);
   }, []);
@@ -28,19 +32,35 @@ function Screen2({ route }) {
       await FileSystem.makeDirectoryAsync(directory, { intermediates: true });
   
       await FileSystem.downloadAsync(imageUrl, fileUri);
-      console.log('Image downloaded:', fileUri);
-      alert('Image Downloaded Successfully.');
+      console.log('Image downloaded:');
+  
+     
+  
+      // Request the MEDIA_LIBRARY permission using expo-file-system's requestPermissionsAsync
+      const { granted } = await MediaLibrary.requestPermissionsAsync();
+      if (!granted) {
+        console.log('Permission denied for media library access.');
+        return;
+      }
+  
+      // Save the file to the device's media library
+      await MediaLibrary.saveToLibraryAsync(fileUri);
+      console.log('Image saved to media library.');
+  
+       // Share the downloaded image using Expo's Sharing API
+       await Sharing.shareAsync(fileUri);
+      // alert('Image Downloaded and Shared Successfully.');
     } catch (error) {
       console.log('Image download error:', error);
       alert('Failed to download the image.');
     }
   };
-
   const showNextImage = async()=>{
     setLoading(true);
     try{
     const data = await fetch (`https://source.unsplash.com/900x1600/?${searchValue}`)
-setImageUrl(data.url);
+// console.log(data);
+    setImageUrl(data.url);
 setLoading(false); 
     }
     catch(err){
@@ -61,13 +81,14 @@ setLoading(false);
 
     }
        
-
+      <View style={styles.btnContainer}>
       <TouchableOpacity style={styles.downloadBtn} onPress={downloadImage}>
         <Text style={styles.downloadText}>Download</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.nextBtn} onPress={showNextImage}>
         <Text style={styles.nextText}>Next</Text>
        </TouchableOpacity>
+       </View>
     </View>
   );
 }
@@ -79,6 +100,8 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     backgroundColor: '#fff',
+    justifyContent:"center",
+    flexDirection:"row",
   },
   image: {
     width: '100%',
@@ -88,24 +111,27 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   downloadBtn:{
-    position:"absolute",
-    bottom:10,
+    // position:"relative",
+    // width:"8%",
+    flexDirection:"row",
     backgroundColor:"black",
     paddingHorizontal:20,
     paddingVertical:10,
-    left:80,
+    justifyContent: 'center',
     borderRadius:10,
     elevation:10,  
 },
 nextBtn:{
-    position:"absolute",
-    bottom:10,
+    // position:"relative",
+    // width:"8%",
+    // height:"auto",
+    flexDirection:"row",
     backgroundColor:"white",
     paddingHorizontal:20,
     paddingVertical:10,
-    right:80,
+    justifyContent: 'center',
     borderRadius:10,
-    elevation:10,
+    elevation:10,  
 },
 downloadText:{
     fontSize:20,
@@ -115,5 +141,14 @@ nextText:{
     fontSize:20,
     color:"black",
     
+},
+btnContainer:{
+
+flexDirection:"row",
+  justifyContent:"space-evenly",
+  width:"100%",
+ 
+  position:"absolute",
+  bottom:15,
 }
 });
